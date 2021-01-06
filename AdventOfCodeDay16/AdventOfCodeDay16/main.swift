d449d3202e31afbf98456ae70e37d58a7828fa71
Vincent-Snow <vincent.van.snow@gmail.com>
2021-01-06 10:10:53 -0700

    progress on day 16

--- AdventOfCodeDay16/AdventOfCodeDay16/main.swift
+++ AdventOfCodeDay16/AdventOfCodeDay16/main.swift
@@ -33,7 +33,7 @@ var testNearBy =    """
 var ticketCounter = 0
 var validTicket = false
 var nearByArray = nearBy.components(separatedBy: "\n")
-//print(nearByArray)
+print(nearByArray)
 //
 //var ticketArray = yourTicket.components(separatedBy: ",")
 //print(ticketArray)
@@ -41,15 +41,19 @@ var nearByArray = nearBy.components(separatedBy: "\n")
 var fieldsArray = fields.components(separatedBy: "\n")
 //print(fieldsArray)
 var rangeArray: [String] = []
+var rangeDict: [String: [String]] = [:]
+var rangeIndex: [String: Int] = [:]
 fieldsLoop: for i in fieldsArray {
     let temp = i.components(separatedBy: ": ")
     let fieldsRanges = temp[1].components(separatedBy: " or ")
     rangeArray.append(fieldsRanges[0])
     rangeArray.append(fieldsRanges[1])
+    rangeDict[temp[0]] = [fieldsRanges[0], fieldsRanges[1]]
 }
-print(rangeArray)
 
-
+for i in rangeDict {
+    print(i)
+}
 mainLoop: for i in nearByArray {
     let ticketArray = i.components(separatedBy: ",")
     ticketLoop: for ticketField in ticketArray {
@@ -59,12 +63,42 @@ mainLoop: for i in nearByArray {
                 continue ticketLoop
             }
         }
+        let index = nearByArray.firstIndex(of: i)!
+        nearByArray.remove(at: index)
         ticketCounter+=Int(ticketField) ?? 0
         continue mainLoop
     }
 }
+//for i in nearByArray {
+//
+//}
+print(nearByArray.count)
+var counter = 0
+var fieldIndex = 0
 
-print(ticketCounter)
+for i in nearByArray {
+    let ticketArray = i.components(separatedBy: ",")
+    tickLoop: for ind in 0..<ticketArray.count {
+    counter = 0
+    let tickInd = Int(ticketArray[ind])!
+        for j in rangeDict {
+            let highLowArray1 = j.value[0].components(separatedBy: "-").map({Int($0)!})
+            let highLowArray2 = j.value[1].components(separatedBy: "-").map({Int($0)!})
+            let lowRange = highLowArray1[0]...highLowArray1[1]
+            let highRange = highLowArray2[0]...highLowArray2[1]
+            if lowRange.contains(tickInd) || highRange.contains(tickInd) {
+                    counter+=1
+                if counter > 1 {
+                    break tickLoop
+                }
+                    fieldIndex = ind
+            }
+        }
+//        if counter > 0
+    }
+}
+
+//print(ticketCounter)
 
 
 
