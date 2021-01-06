48ca52422a5c82e98e6ef34c01daf931e5bd326f
Vincent-Snow <vincent.van.snow@gmail.com>
2021-01-05 20:53:26 -0700

    Code does not look pretty but day 11 pt 2 is solved

--- AdventOfCodeDay11/AdventOfCodeDay11/main.swift
+++ AdventOfCodeDay11/AdventOfCodeDay11/main.swift
@@ -20,38 +20,134 @@ var testInput = """
         L.LLLLLL.L
         L.LLLLL.LL
         """
-var initialSeatRows = testInput.components(separatedBy: "\n")
+var otrosInput = """
+                .......#.
+                ...#.....
+                .#.......
+                .........
+                ..#L....#
+                ....#....
+                .........
+                #........
+                ...#.....
+                """
+var yetAnotherInput = """
+                .............
+                .L.L.#.#.#.#.
+                .............
+                """
+var oneLastInput = """
+                .##.##.
+                #.#.#.#
+                ##...##
+                ...L...
+                ##...##
+                #.#.#.#
+                .##.##.
+                """
+
+var initialSeatRows = input.components(separatedBy: "\n")
 //print(initialSeatRows)
 for i in 0...initialSeatRows.count-1 {
     seatRows.append(initialSeatRows[i].map { String($0) })
 }
-print(seatRows[0].count-1)
+
 var seatCounter = 0
 var newSeatConfig = seatRows
 var counter = 0
-func eachSeat(row: Int, col: Int) {
-    counter = 0
-    eachSeat: for x in 0...2 {
-        for y in 0...2 {
-            //print(x-1, y-1)
-            if row + x - 1 < 0 || row + x - 1 > seatRows.count - 1 {
-                continue
-            } else if col + y - 1 < 0  || col + y - 1 > seatRows[row].count - 1 {
-                continue
-            }
-            if x - 1 == 0 && y - 1 == 0 {
-                continue
-            }
-            if seatRows[row+(x-1)][col+(y-1)] == "#" {
-                counter+=1
+var probcounter = 0
+
+func doThis(row:Int,col:Int,tempx:Int,tempy:Int) -> Bool {
+    if seatRows[row+tempx][col+tempy] == "#" {
+        counter+=1
+        return true
+    } else if seatRows[row+tempx][col+tempy] == "L" {
+        return true
+    } else {
+        return false
+    }
+}
+
+    func eachSeat(row: Int, col: Int) {
+        eachSeat: for x in 0...2 {
+            innerLoop: for y in 0...2 {
+                var tempx = x-1
+                var tempy = y-1
+                
+                while row + tempx > -2 && row + tempx < seatRows.count && col + tempy > -2  && col + tempy < seatRows[row].count  {
+                    if row + tempx < 0 || row + tempx > seatRows.count - 1 {
+                        continue innerLoop
+                    } else if col + tempy < 0  || col + tempy > seatRows[row].count - 1 {
+                        continue innerLoop
+                    }
+                    switch (x-1, y-1) {
+                    case (-1,-1):
+                        if doThis(row: row, col: col, tempx: tempx, tempy: tempy) {
+                            continue innerLoop
+                        } else {
+                            tempx-=1
+                            tempy-=1
+                        }
+                    case (-1,0):
+                        if doThis(row: row, col: col, tempx: tempx, tempy: tempy) {
+                            continue innerLoop
+                        } else {
+                            tempx-=1
+                        }
+                    case (0,-1):
+                        if doThis(row: row, col: col, tempx: tempx, tempy: tempy) {
+                            continue innerLoop
+                        } else {
+                            tempy-=1
+                        }
+                    case (1,-1):
+                        if doThis(row: row, col: col, tempx: tempx, tempy: tempy) {
+                            continue innerLoop
+                        } else {
+                            tempx+=1
+                            tempy-=1
+                        }
+                    case (0,0):
+                        continue innerLoop
+                    case (-1,1):
+                        if doThis(row: row, col: col, tempx: tempx, tempy: tempy) {
+                            continue innerLoop
+                        } else {
+                            tempx-=1
+                            tempy+=1
+                        }
+                    case (1,0):
+                        if doThis(row: row, col: col, tempx: tempx, tempy: tempy) {
+                            continue innerLoop
+                        } else {
+                            tempx+=1
+                        }
+                    case (0,1):
+                        if doThis(row: row, col: col, tempx: tempx, tempy: tempy) {
+                            continue innerLoop
+                        } else {
+                            tempy+=1
+                        }
+                    case (1,1):
+                        if doThis(row: row, col: col, tempx: tempx, tempy: tempy) {
+                            continue innerLoop
+                        } else {
+                            tempx+=1
+                            tempy+=1
+                        }
+                    default:
+                        continue innerLoop
+                }
             }
         }
     }
 }
+
 func problem() {
 
     rowLoop: for row in 0...seatRows.count-1 {
         colLoop: for col in 0...seatRows[row].count-1 {
+            counter=0
             if seatRows[row][col] == "L" {
                 eachSeat(row: row, col: col)
                 if counter == 0 {
@@ -59,17 +155,14 @@ func problem() {
                 }
             } else if seatRows[row][col] == "#" {
                 eachSeat(row: row, col: col)
-                if counter >= 4 {
+                if counter >= 5 {
                     newSeatConfig[row][col] = "L"
                     //print("is it ever getting here")
                 }
             }
         }
     }
-//    print("break")
-//    for i in newSeatConfig {
-//        //print(i)
-//    }
+
     if newSeatConfig == seatRows {
         print("the end")
         for i in newSeatConfig {
@@ -83,11 +176,88 @@ func problem() {
         
         return
     }
+
     seatRows = newSeatConfig
     problem()
 }
 problem()
 print(seatCounter)
+
+
+
+
+
+func pt1(initialSeatRows:[String]) {
+    for i in 0...initialSeatRows.count-1 {
+        seatRows.append(initialSeatRows[i].map { String($0) })
+    }
+    print(seatRows[0].count-1)
+    var counter = 0
+    func eachSeat(row: Int, col: Int) {
+        counter = 0
+        eachSeat: for x in 0...2 {
+            for y in 0...2 {
+                //print(x-1, y-1)
+
+                if row + x - 1 < 0 || row + x - 1 > seatRows.count - 1 {
+                    continue
+                } else if col + y - 1 < 0  || col + y - 1 > seatRows[row].count - 1 {
+                    continue
+                }
+                if x - 1 == 0 && y - 1 == 0 {
+                    continue
+                }
+
+                    if seatRows[row+(x-1)][col+(y-1)] == "#" {
+                        counter+=1
+                    }
+            }
+        }
+    }
+    func problem2() {
+
+        rowLoop: for row in 0...seatRows.count-1 {
+            colLoop: for col in 0...seatRows[row].count-1 {
+                if seatRows[row][col] == "L" {
+                    eachSeat(row: row, col: col)
+                    if counter == 0 {
+                        newSeatConfig[row][col] = "#"
+                    }
+                } else if seatRows[row][col] == "#" {
+                    eachSeat(row: row, col: col)
+                    if counter >= 4 {
+                        newSeatConfig[row][col] = "L"
+                        //print("is it ever getting here")
+                    }
+                }
+            }
+        }
+    //    print("break")
+    //    for i in newSeatConfig {
+    //        //print(i)
+    //    }
+        if newSeatConfig == seatRows {
+            print("the end")
+            for i in newSeatConfig {
+                //print(i)
+                for j in i {
+                    if j == "#" {
+                        seatCounter+=1
+                    }
+                }
+            }
+            
+            return
+        }
+        seatRows = newSeatConfig
+        problem2()
+    }
+    problem2()
+    print(seatCounter)
+}
+
+
+
 //if corner check 3
 //if edge check 5
 //if middle check 8
