4610648979980b799c73abf08506dea1a27b5a46
Vincent-Snow <vincent.van.snow@gmail.com>
2021-01-18 00:10:08 -0700

    day 24 part one solved!

--- AdventOfCodeDay24/AdventOfCodeDay24/PuzzleInput.swift
+++ AdventOfCodeDay24/AdventOfCodeDay24/PuzzleInput.swift
@@ -22,6 +22,39 @@ extension StringProtocol {
     subscript(range: PartialRangeUpTo<Int>) -> SubSequence { self[..<index(startIndex, offsetBy: range.upperBound)] }
 }
 
+protocol IntPairConvertible {
+    init(_ pair: (Int, Int))
+}
+
+struct IntPairStruct: Hashable, IntPairConvertible {
+    let int0: Int
+    let int1: Int
+    
+    init(_ pair: (Int, Int)) {
+        self.int0 = pair.0
+        self.int1 = pair.1
+    }
+}
+
+extension Dictionary where Key: IntPairConvertible {
+    subscript (key: (Int, Int)) -> Value? {
+        get {
+            return self[Key(key)]
+        }
+        set {
+            self[Key(key)] = newValue
+        }
+    }
+    subscript (key0: Int, key1: Int) -> Value? {
+        get {
+            return self[Key((key0, key1))]
+        }
+        set {
+            self[Key((key0, key1))] = newValue
+        }
+    }
+}
+
 let input = """
 swswswswneswswwwswnewswswweswnwsww
 nwwewenenwnenwnwnwnwneswnwswnwnwswswnw
--- AdventOfCodeDay24/AdventOfCodeDay24/main.swift
+++ AdventOfCodeDay24/AdventOfCodeDay24/main.swift
@@ -30,18 +30,56 @@ neswnwewnwnwseenwseesewsenwsweewe
 wseweeenwnesenwwwswnew
 """
 
+var coordDict: [IntPairStruct:String] = [:]
 let inputArray = testInput.components(separatedBy: "\n")
-print(inputArray)
+let test = ["nwwswee"]
 for i in inputArray {
+    var x = 0.0
+    var y = 0.0
     for j in 0..<i.count {
         if j > 0  && (i[j-1] == "s" || i[j-1] == "n") {
             continue
         }
+        let dir: String
         if i[j] == "s" || i[j] == "n" {
-            print("\(i[j])\(i[j+1])")
+            dir = "\(i[j])\(i[j+1])"
+            //print("\(i[j])\(i[j+1])")
         } else {
-            print(i[j])
+            dir = String(i[j])
+            //print(i[j])
+        }
+        switch dir {
+        case "nw":
+            x-=0.5;y+=0.5;
+        case "w":
+            x-=1
+        case "sw":
+            x-=0.5;y-=0.5;
+        case "se":
+            x+=0.5;y-=0.5
+        case "e":
+            x+=1
+        case "ne":
+            x+=0.5;y+=0.5;
+        default:
+            print("broke")
+            break
         }
     }
-    print("\n new line \n")
+    let xy = (Int(x*10),Int(y*10))
+    if coordDict[xy] == nil {
+        coordDict[xy] = "black"
+    } else if coordDict[xy] == "black" {
+        coordDict[xy] = "white"
+    } else if coordDict[xy] == "white" {
+        coordDict[xy] = "black"
+    }
+    //print(xy)
 }
+for i in coordDict {
+    print(i)
+}
+let item = coordDict.filter({$0.value == "black"})
+print(item.count)
+
+
