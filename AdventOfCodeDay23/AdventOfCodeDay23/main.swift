da65f5dc18acbed06d0dad0745d13c141a3039b2
Vincent-Snow <vincent.van.snow@gmail.com>
2021-01-18 23:48:56 -0700

    Day 23 part 1 solved, working on pt 2

--- AdventOfCodeDay23/AdventOfCodeDay23/main.swift
+++ AdventOfCodeDay23/AdventOfCodeDay23/main.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 
+let start = Date()
 var pickUpArray: [Int] = []
 var input2 = [1,9,8,7,5,3,4,6,2]
 var input = [3,8,9,1,2,5,4,6,7]
@@ -19,25 +20,29 @@ var destIndex = 0
 var hasLowerDest = false
 var isPickUpAtEndOfArray = false
 //print(input, input[currentIndex])
-for _ in 1...10 {
-    print(input, currentVal)
+for i in 10...1000000 {
+    input.append(i)
+}
+for k in 1...10000000 {
+    //print(input, currentVal)
     
     // pick up cups and put them in their own array
-    if pickUpIndex + 3 >= input.count {
-        isPickUpAtEndOfArray = true
-    }
+//    if pickUpIndex + 3 >= input.count {
+//        isPickUpAtEndOfArray = true
+//    }
     for _ in 1...3 {
-        print(input.count, pickUpIndex)
-        if pickUpIndex >= input.count-1 {
-            pickUpArray.insert(input.remove(at: 0), at: 0)
+        //print(input.count, pickUpIndex)
+        if pickUpIndex >= input.count {
+            pickUpArray.append(input.remove(at: 0))
         } else {
             //print(input)
             pickUpArray.append(input.remove(at: pickUpIndex))
         }
     }
+    //print(pickUpArray)
     // set dest
     for x in 1...3 {
-        if input.contains(currentVal-x) {
+        if !pickUpArray.contains(currentVal-x) {
             dest = currentVal-x
             hasLowerDest = true
             break
@@ -46,11 +51,12 @@ for _ in 1...10 {
     if !hasLowerDest {
         dest = input.max()!
     }
-    print("dest", dest, input)
+    //print("dest", dest, input)
     
     destIndex = input.firstIndex(of: dest) ?? 0
     //print(currentVal, currentIndex, dest, destIndex, input, input.count, pickUpArray)
-    print("dest index", destIndex)
+    //print("dest index", destIndex)
+    
     //put picked up cups to the right of dest
     for i in pickUpArray.reversed() {
         if destIndex == input.count-1 {
@@ -61,7 +67,7 @@ for _ in 1...10 {
     }
     
     // set new current cup
-    print(currentIndex, currentVal)
+    //print(currentIndex, currentVal)
     currentIndex = input.firstIndex(of: currentVal) ?? 0
     if currentIndex == input.count-1 {
         currentIndex = 0
@@ -73,4 +79,8 @@ for _ in 1...10 {
     currentVal = input[currentIndex]
     pickUpIndex = currentIndex+1
     pickUpArray = []
+//    print(Date().timeIntervalSince(start))
 }
+//print(input, currentVal)
+let x = input.firstIndex(of: 1)
+print(input[x!+1]*input[x!+2], Date().timeIntervalSince(start))
